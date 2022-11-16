84e16d1163b9e4febc556e5f5a260711da3e137d
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-11-17 03:58:11 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+//middleware
+app.use(express.json());
+app.use(cors());
+/* //routes
+const recordsRoute=require('./routes/record.route')
+const fileRoute=require('./routes/file.route') */
+
+app.get("/", (req, res) => {
+  res.send("Route is working! YaY!");
+});
+
+/* // route colling
+app.use("/api/v1/records",recordsRoute)
+app.use("/api/v1/file",fileRoute) */
+
+
+
+module.exports = app;
